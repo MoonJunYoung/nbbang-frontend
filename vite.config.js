7bdb015656abd002b5c75fe73704797634466459
dnfbd <dnfbd@naver.com>
2024-12-28 17:10:59 +0900

    feat:메타데이터 업데이트

--- vite.config.js
+++ vite.config.js
@@ -5,32 +5,5 @@ import { VitePWA } from 'vite-plugin-pwa';
 export default defineConfig({
   plugins: [
     react(),
-    VitePWA({
-      registerType: 'prompt',
-      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
-      manifest: {
-        name: 'Nbbang',
-        short_name: 'Nbbang',
-        description: '빠른 정산, 원클릭 송금',
-        theme_color: '#ffffff',
-        start_url: '/',
-        icons: [
-          {
-            src: 'images/nbbang.png',
-            sizes: '192x192',
-            type: 'image/png'
-          },
-          {
-            src: 'images/nbbang.png',
-            sizes: '512x512',
-            type: 'image/png',
-            purpose: 'any maskable'
-          }
-        ]
-      },
-      workbox: {
-        globPatterns: ['**/*.{js,css,html,ico,png,svg}']
-      }
-    })
   ]
 });
