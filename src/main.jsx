2da1b36802eb1e0c77a1b9df25881937a580ca8b
MoonJunYoung <kkunyeot0729@gmail.com>
2025-02-04 20:44:49 +0900

    초기서비스워커 코드를 원복한다

--- src/main.jsx
+++ src/main.jsx
@@ -1,20 +1,20 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
 
 if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker
-      .register('/service-worker.js')
-      .then((reg) => {
-        console.log('Self-destroy SW registered:', reg);
-      })
-      .catch((err) => {
-        console.error('SW registration failed:', err);
-      });
-  });
+    window.addEventListener('load', () => {
+        navigator.serviceWorker
+            .register('/sw.js')
+            .then((registration) => {
+                console.log('SW registered: ', registration);
+            })
+            .catch((registrationError) => {
+                console.log('SW registration failed: ', registrationError);
+            });
+    });
 }
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
