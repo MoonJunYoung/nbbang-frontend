27ec39f4e038ab50076747c311081d35591a4b16
MoonJunYoung <kkunyeot0729@gmail.com>
2025-02-04 20:54:46 +0900

    test:서비스워커 레지스터가 빌드시 설정될수있도록 수정한다

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,43 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
-
+import { VitePWA } from 'vite-plugin-pwa';
 export default defineConfig({
-    plugins: [react()],
+    plugins: [
+        react(),
+        VitePWA({
+            registerType: 'prompt',
+            includeAssets: [
+                'favicon.ico',
+                'apple-touch-icon.png',
+                'masked-icon.svg',
+            ],
+            manifest: {
+                name: 'Nbbang',
+                short_name: 'Nbbang',
+                description: '빠른 정산, 원클릭 송금',
+                theme_color: '#ffffff',
+                start_url: '/',
+                icons: [
+                    {
+                        src: 'images/nbbang.png',
+                        sizes: '192x192',
+                        type: 'image/png',
+                    },
+                    {
+                        src: 'images/nbbang.png',
+                        sizes: '512x512',
+                        type: 'image/png',
+                        purpose: 'any maskable',
+                    },
+                ],
+            },
+            workbox: {
+                globPatterns: ['**/*.{js,css,html,ico,png,svg}'],
+                maximumFileSizeToCacheInBytes: 3 * 1024 * 1024, // 3 MiB로 설정
+            },
+        }),
+    ],
     resolve: {
         alias: {
             '@': path.resolve(__dirname, './src'),
