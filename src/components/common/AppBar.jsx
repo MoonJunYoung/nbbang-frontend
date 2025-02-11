dc5437242cd4a8da25b8c5d7d4ba44c5c8f5f9ff
dnfbd <dnfbd@naver.com>
2025-02-11 18:29:49 +0900

    fix:앱bar 및 계산기 기능 수정

--- src/components/common/AppBar.jsx
+++ src/components/common/AppBar.jsx
@@ -2,15 +2,26 @@ import { useEffect, useState } from 'react';
 
 const AppBar = () => {
     const [isAndroid, setIsAndroid] = useState(false);
+    const [hideAppBar, setHideAppBar] = useState(false);
 
     useEffect(() => {
         const userAgent = navigator.userAgent.toLowerCase();
         if (userAgent.includes('android')) {
             setIsAndroid(true);
         }
+
+        const isAppInstalled = localStorage.getItem('app_installed') === 'true';
+        if (isAppInstalled) {
+            setHideAppBar(true);
+        }
     }, []);
 
-    if (!isAndroid) return null;
+    const handleAppOpen = () => {
+        localStorage.setItem('app_installed', 'true');
+        setHideAppBar(true);
+    };
+
+    if (!isAndroid || hideAppBar) return null;
 
     return (
         <div className="bg-[#3167fc] flex items-center p-3 justify-between">
@@ -20,9 +31,12 @@ const AppBar = () => {
                     앱에서 더욱 간편하게 이용해보세요!
                 </span>
             </div>
-            <span className="text-black bg-white rounded-xl font-semibold px-2 py-1 text-sm md:text-base max-[360px]:text-[11px]">
+            <button
+                onClick={handleAppOpen}
+                className="text-black bg-white rounded-xl font-semibold px-2 py-1 text-sm md:text-base max-[360px]:text-[11px]"
+            >
                 앱에서 이용하기
-            </span>
+            </button>
         </div>
     );
 };
